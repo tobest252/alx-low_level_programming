b762de99cae421bf4bb5a40a22ab480b6a33e921
tobest242 <tobest252@gmail.com>
2022-12-09 16:42:25 -0800

    Taks 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>I
+/**
+ * main - returns alphabetd both lower and upper cases
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
